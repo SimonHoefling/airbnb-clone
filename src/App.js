360fd3f6537fd2e4adf595dac8689400e93448ed
Simon Hoefling <simon@hoeflia.eu>
2023-04-03 17:20:16 +0800

    Added marker to map

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import GoogleMapReact from 'google-map-react';
 
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      selectedFlat: null
     };
   }
 
@@ -23,10 +25,24 @@ class App extends Component {
       })
   }
 
+  selectFlat = (flat) => {
+    console.log(flat);
+    this.setState({
+      selectedFlat: flat
+    })
+  }
+
   render() {
     let center = {
       lat: 48.8566,
       lng: 2.3522
+    }
+
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      }
     }
 
     return (
@@ -36,7 +52,10 @@ class App extends Component {
           </div>
           <div className="flats">
             {this.state.flats.map((flat) => {
-              return <Flat flat={flat} />
+              return <Flat
+                key={flat.name}
+                flat={flat}
+                selectFlat={this.selectFlat} />
             })}
           </div>
         </div>
@@ -45,6 +64,14 @@ class App extends Component {
             center={center}
             zoom={11}
           >
+          {this.state.flats.map((flat) => {
+            return <Marker
+              key={flat.name}
+              lat={flat.lat}
+              lng={flat.lng}
+              text={flat.price}
+              selected={flat === this.state.selectedFlat} />
+          })}
           </GoogleMapReact>
         </div>
       </div>
